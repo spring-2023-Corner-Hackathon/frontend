35c16d91be5c0aa6d705104e9f6a156c056c8833
rain_hyeon <iiilee0907@naver.com>
2023-01-21 17:57:00 +0900

    Homefragment spinner(최신순, 이름순) 작성 완료

--- app/src/main/java/com/example/photobook/HomeFragment.kt
+++ app/src/main/java/com/example/photobook/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.example.photobook
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.SpinnerAdapter
 import com.example.photobook.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -23,6 +26,7 @@ class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var binding : FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,13 +41,49 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
-
 
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //spinner 적용
+        val spinner = binding.sort
+        val menu = resources.getStringArray(R.array.spinner_array)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, menu)
+        spinner.adapter = adapter
+
+        //spinner 선택 이벤트 처리
+        spinner.setSelection(1)
+        spinner.onItemClickListener = object : AdapterView.OnItemSelectedListener,
+            AdapterView.OnItemClickListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                when(p2){
+                    //최신순 선택
+                    0 -> {
+                        //아이디가 작은 것부터 recyclerview 나열(백)
+                    }
+                    //이름순 선택
+                    1 -> {
+                        //이름순으로 recyclerview 나열(백)
+                    }
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                TODO("Not yet implemented")
+            }
+        }
+
+    }
+
+        companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
