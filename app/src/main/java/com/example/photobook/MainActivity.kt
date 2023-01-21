0153a6689f3e5e020acbf44f370fc17e1077ccb3
rain_hyeon <iiilee0907@naver.com>
2023-01-21 17:08:44 +0900

    하단 네비게이션 바 설정(Fragment로 화면전환 - 캘린더, 홈, 마이페이지)

--- app/src/main/java/com/example/photobook/MainActivity.kt
+++ app/src/main/java/com/example/photobook/MainActivity.kt
@@ -5,6 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.example.photobook.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+
+
+private const val TAG_CALENDER = "calender_fragment"
+private const val TAG_HOME = "home_fragment"
+private const val TAG_MY_PAGE = "my_page_fragment"
 
 class MainActivity : AppCompatActivity() {
     lateinit var mainbinding: ActivityMainBinding
@@ -14,15 +21,61 @@ class MainActivity : AppCompatActivity() {
         mainbinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainbinding.root)
 
-        //spinner 내용 넣기
-        val data = resources.getStringArray(R.array.spinner_array)
-        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
-        mainbinding.sort.adapter = adapter
 
-        mainbinding.goLogin.setOnClickListener{
-            val intent = Intent(applicationContext, LoginActivity::class.java)
-            startActivity(intent)
+        //navigationbar
+        setFragment(TAG_HOME,HomeFragment())
+
+        mainbinding.navigationView.setOnItemSelectedListener {  item ->
+            when(item.itemId) {
+                R.id.calendarFragment -> setFragment(TAG_CALENDER, CalendarFragment())
+                R.id.homeFragment -> setFragment(TAG_HOME, HomeFragment())
+                R.id.myPageFragment -> setFragment(TAG_MY_PAGE, MyPageFragment())
+            }
+            true
+        }
+    }
+
+    private fun setFragment(tag: String, fragment: Fragment) {
+        val manager: FragmentManager = supportFragmentManager
+        val fragTransaction = manager.beginTransaction()
+
+        if(manager.findFragmentByTag(tag) == null){
+            fragTransaction.add(R.id.mainFrameLayout, fragment, tag)
         }
 
+        val calender = manager.findFragmentByTag(TAG_CALENDER)
+        val home = manager.findFragmentByTag(TAG_HOME)
+        val myPage = manager.findFragmentByTag(TAG_MY_PAGE)
+
+        if(calender != null){
+            fragTransaction.hide(calender)
+        }
+
+        if(home != null){
+            fragTransaction.hide(home)
+        }
+
+        if(myPage != null){
+            fragTransaction.hide(myPage)
+        }
+
+        if(tag == TAG_HOME){
+            if(home != null){
+                fragTransaction.show(home)
+            }
+        }
+        else if(tag == TAG_CALENDER){
+            if(calender != null){
+                fragTransaction.show(calender)
+            }
+        }
+        else if(tag == TAG_MY_PAGE){
+            if(myPage != null){
+                fragTransaction.show(myPage)
+            }
+        }
+
+        fragTransaction.commitAllowingStateLoss()
     }
+
 }
