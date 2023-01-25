f506f1449efe6ddee5c7ab023e8c9b26485a54e4
sumin <ski03257@naver.com>
2023-01-25 14:37:19 +0900

    음악 선택

---
+++ app/src/main/java/com/example/photobook/ChooseMusic.kt
@@ -0,0 +1,170 @@
+package com.example.photobook
+
+import android.content.Intent
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.SystemClock
+import android.widget.SeekBar
+import com.example.photobook.databinding.ActivityChooseMusicBinding
+import java.text.SimpleDateFormat
+
+class ChooseMusic : AppCompatActivity() {
+    lateinit var selectedMp3: String // 현재 선택된 mp3 파일
+    // mp3 player 객체 생성
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_choose_music3)
+
+        var mPlayer = MediaPlayer.create(this, R.raw.colors)
+        val binding = ActivityChooseMusicBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val musicCategory = intent.getStringExtra("musicCategory")
+
+        binding.selectedMusicCategory.text = musicCategory
+
+        binding.musicRg.setOnCheckedChangeListener { radioGroup, checkedId ->
+            when(checkedId) {
+                R.id.colors -> {
+                    selectedMp3 = "colors"
+
+                    if (mPlayer.isPlaying) {
+                        mPlayer.stop()
+                    }
+                    mPlayer = MediaPlayer.create(this, R.raw.colors)
+                    mPlayer.start()
+
+                    // 시크바 구현
+                    object : Thread() {
+                        var timeFormat = SimpleDateFormat("mm:ss")
+                        override fun run() {
+                            super.run()
+                            if (mPlayer == null)
+                                return
+                            binding.seekBar.max = mPlayer.duration
+                            while (mPlayer.isPlaying) {
+                                runOnUiThread {
+                                    binding.seekBar.progress = mPlayer.currentPosition
+                                    binding.textView2.text =
+                                        "" + timeFormat.format(mPlayer.currentPosition)
+                                }
+                                SystemClock.sleep(200)
+                            }
+                        }
+                    }.start()
+
+                }
+                R.id.lamour -> {
+                    selectedMp3 = "lamour"
+
+                    if (mPlayer.isPlaying) {
+                        mPlayer.stop()
+                    }
+                    mPlayer = MediaPlayer.create(this, R.raw.lamour)
+                    mPlayer.start()
+
+                    // 시크바 구현
+                    object : Thread() {
+                        var timeFormat = SimpleDateFormat("mm:ss")
+                        override fun run() {
+                            super.run()
+                            if (mPlayer == null)
+                                return
+                            binding.seekBar.max = mPlayer.duration
+                            while (mPlayer.isPlaying) {
+                                runOnUiThread {
+                                    binding.seekBar.progress = mPlayer.currentPosition
+                                    binding.textView2.text =
+                                        "" + timeFormat.format(mPlayer.currentPosition)
+                                }
+                                SystemClock.sleep(200)
+                            }
+                        }
+                    }.start()
+                }
+                R.id.flamingo -> {
+                    selectedMp3 = "flamingo"
+
+                    if (mPlayer.isPlaying) {
+                        mPlayer.stop()
+                    }
+                    mPlayer = MediaPlayer.create(this, R.raw.flamingo)
+                    mPlayer.start()
+
+                    // 시크바 구현
+                    object : Thread() {
+                        var timeFormat = SimpleDateFormat("mm:ss")
+                        override fun run() {
+                            super.run()
+                            if (mPlayer == null)
+                                return
+                            binding.seekBar.max = mPlayer.duration
+                            while (mPlayer.isPlaying) {
+                                runOnUiThread {
+                                    binding.seekBar.progress = mPlayer.currentPosition
+                                    binding.textView2.text =
+                                        "" + timeFormat.format(mPlayer.currentPosition)
+                                }
+                                SystemClock.sleep(200)
+                            }
+                        }
+                    }.start()
+                }
+                R.id.weekend -> {
+                    selectedMp3 = "weekend"
+
+                    if (mPlayer.isPlaying) {
+                        mPlayer.stop()
+                    }
+                    mPlayer = MediaPlayer.create(this, R.raw.weekend)
+                    mPlayer.start()
+
+                    // 시크바 구현
+                    object : Thread() {
+                        var timeFormat = SimpleDateFormat("mm:ss")
+                        override fun run() {
+                            super.run()
+                            if (mPlayer == null)
+                                return
+                            binding.seekBar.max = mPlayer.duration
+                            while (mPlayer.isPlaying) {
+                                runOnUiThread {
+                                    binding.seekBar.progress = mPlayer.currentPosition
+                                    binding.textView2.text =
+                                        "" + timeFormat.format(mPlayer.currentPosition)
+                                }
+                                SystemClock.sleep(200)
+                            }
+                        }
+                    }.start()
+                }
+            }
+        }
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser:Boolean) {
+                if(fromUser) {
+                    mPlayer.seekTo(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                //TODO("Not yet implemented")
+            }
+        })
+
+        binding.coverBack.setOnClickListener {
+            if (mPlayer.isPlaying) {
+                mPlayer.stop()
+            }
+            val intent = Intent(this, MusicCategory::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/photobook/MusicCategory.kt
@@ -0,0 +1,96 @@
+package com.example.photobook
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.photobook.databinding.ActivityMusicCategoryBinding
+
+class MusicCategory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_music_category)
+
+        var musicCategory = "null"
+        var musicCategoryKorean = "null"
+        val binding = ActivityMusicCategoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.musicCategory1.setOnCheckedChangeListener { button, isChecked ->
+            if(isChecked) {
+                binding.musicCategory2.isChecked = false
+                binding.musicCategory3.isChecked = false
+                binding.musicCategory4.isChecked = false
+                binding.musicCategory5.isChecked = false
+                binding.musicCategory6.isChecked = false
+                musicCategory = "travel"
+                musicCategoryKorean = "여행"
+            }
+        }
+        binding.musicCategory2.setOnCheckedChangeListener { button, isChecked ->
+            if(isChecked) {
+                binding.musicCategory1.isChecked = false
+                binding.musicCategory3.isChecked = false
+                binding.musicCategory4.isChecked = false
+                binding.musicCategory5.isChecked = false
+                binding.musicCategory6.isChecked = false
+                musicCategory = "tong"
+                musicCategoryKorean = "통통튀는"
+            }
+        }
+        binding.musicCategory3.setOnCheckedChangeListener { button, isChecked ->
+            if(isChecked) {
+                binding.musicCategory1.isChecked = false
+                binding.musicCategory2.isChecked = false
+                binding.musicCategory4.isChecked = false
+                binding.musicCategory5.isChecked = false
+                binding.musicCategory6.isChecked = false
+                musicCategory = "refined"
+                musicCategoryKorean = "세련된"
+            }
+        }
+        binding.musicCategory4.setOnCheckedChangeListener { button, isChecked ->
+            if(isChecked) {
+                binding.musicCategory1.isChecked = false
+                binding.musicCategory2.isChecked = false
+                binding.musicCategory3.isChecked = false
+                binding.musicCategory5.isChecked = false
+                binding.musicCategory6.isChecked = false
+                musicCategory = "calm"
+                musicCategoryKorean = "잔잔한"
+            }
+        }
+        binding.musicCategory5.setOnCheckedChangeListener { button, isChecked ->
+            if(isChecked) {
+                binding.musicCategory1.isChecked = false
+                binding.musicCategory2.isChecked = false
+                binding.musicCategory3.isChecked = false
+                binding.musicCategory4.isChecked = false
+                binding.musicCategory6.isChecked = false
+                musicCategory = "sad"
+                musicCategoryKorean = "슬픈"
+            }
+        }
+        binding.musicCategory6.setOnCheckedChangeListener { button, isChecked ->
+            if(isChecked) {
+                binding.musicCategory1.isChecked = false
+                binding.musicCategory2.isChecked = false
+                binding.musicCategory3.isChecked = false
+                binding.musicCategory4.isChecked = false
+                binding.musicCategory5.isChecked = false
+                musicCategory = "pop"
+                musicCategoryKorean = "팝송"
+            }
+        }
+        binding.coverNext.setOnClickListener {
+            val intent = Intent(this, ChooseMusic::class.java)
+            intent.putExtra("musicCategory", musicCategoryKorean)
+            startActivity(intent)
+        }
+        binding.coverBack.setOnClickListener {
+            val intent = Intent(this, MakeActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
